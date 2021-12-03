7915766aa985abc250dfdb448a4b992b3c74fa81
oxygenism <git@horchner.dev>
2021-12-04 00:02:57 +0100

    day4 preperations

--- run.php
+++ run.php
@@ -20,7 +20,8 @@ foreach ($handle as $line) {
 $days = [
     new App\Advent\Days\Day1(),
     new App\Advent\Days\Day2(),
-    new App\Advent\Days\Day3()
+    new App\Advent\Days\Day3(),
+    new App\Advent\Days\Day4(),
 ];
 
 foreach ($days as $day) {
---
+++ src/Days/Day4.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Advent\Days;
+
+use JetBrains\PhpStorm\Pure;
+use App\Advent\Utility\DataService;
+
+class Day4
+{
+    private DataService $dataService;
+
+    #[Pure] public function __construct()
+    {
+        $this->dataService = new DataService();
+    }
+
+    public function runA()
+    {
+        $handle = $this->dataService->read("day0_test.txt");
+
+
+//        return $count;
+    }
+
+    public function runB()
+    {
+        $handle = $this->dataService->read("day0_test.txt");
+
+
+
+//        return $count;
+    }
+}
