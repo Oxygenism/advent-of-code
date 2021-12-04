45267f51d94789a122e403744c77a323a6661bfe
oxygenism <git@horchner.dev>
2021-12-04 12:59:12 +0100

    updates

--- src/Days/Day0.php
+++ src/Days/Day0.php
@@ -30,4 +30,16 @@ class Day0
 
 //        return $count;
     }
+
+    public function sanitizeInput($input) {
+        //https://www.w3schools.com/php/php_ref_filter.asp might help.. someday
+        //https://www.w3schools.com/php/php_ref_string.asp string manipulation
+        //https://www.w3schools.com/php/php_ref_array.asp array stuff
+        //https://www.w3schools.com/php/php_ref_math.asp math stuff
+
+
+        $trimmed = ltrim($input); //left trim
+        $trimmed = rtrim($trimmed); //right trim
+        return preg_replace('!\s+!', ',', $input);
+    }
 }
