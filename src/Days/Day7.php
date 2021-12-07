a83d80a038b08269c270bb76e4a4023d20409a54
oxygenism <oxygenism@horchner.dev>
2021-12-07 07:37:14 +0100

    day7: with a really smart part 2

--- src/Days/Day7.php
+++ src/Days/Day7.php
@@ -9,7 +9,7 @@ class Day7
 {
     private DataService $dataService;
 
-    #[Pure] public function __construct()
+    public function __construct()
     {
         $this->dataService = new DataService();
     }
@@ -21,56 +21,48 @@ class Day7
         $sanitizedString = $this->sanitizeInput($inputString);
         $inputArray = explode(',', $sanitizedString);
         $positions = DataService::getIntegerArray($inputArray);
-        $countPositions = array_count_values($positions);
 
         $func = function(&$item, $key, $mostFreqPosition) {
-            if ($item > $mostFreqPosition) {
-                $diff = abs($item - $mostFreqPosition);
-            } else {
-                $diff = abs($mostFreqPosition - $item);
-            }
-
+            $diff = abs($item - $mostFreqPosition);
             $item = $diff;
-
         };
 
-        $leastFuel = PHP_INT_MAX;
-        print_r($countPositions);
-        foreach ($countPositions as $check) {
-            $tempPositions = $positions;
-            array_walk($tempPositions, $func, $check);
-            $sum = array_sum($tempPositions);
-            if ($sum < $leastFuel) {
-                $leastFuel = $sum;
-            }
-        }
-
-        return $leastFuel;
+        return $this->getLeastFuelConsumption($positions, $func);
     }
 
     public function runB()
     {
-        $handle = $this->dataService->read("day7_test.txt");
+        $handle = $this->dataService->read("day7.txt");
         $inputString = $handle->current();
         $sanitizedString = $this->sanitizeInput($inputString);
         $inputArray = explode(',', $sanitizedString);
         $positions = DataService::getIntegerArray($inputArray);
-        $countPositions = array_count_values($positions);
-        $maxKey = array_keys($countPositions, max($countPositions));
-        $mostFreqPosition = $maxKey[0];
+        $avg = floor(array_sum($positions) / count($positions));
 
-        $func = function(&$item, $key, $mostFreqPosition) {
-            if ($item > $mostFreqPosition) {
-                $diff = abs($item - $mostFreqPosition);
-            } else {
-                $diff = abs($mostFreqPosition - $item);
-            }
-
-            $item = $diff;
-
+        $func = function(&$item, $key, $avg) {
+            $diff = abs($item - $avg);
+            $item = array_sum(range(0, $diff));
         };
-        array_walk($positions, $func, $mostFreqPosition);
+
+        array_walk($positions, $func, $avg);
         return array_sum($positions);
+    }
+
+    function getLeastFuelConsumption($positions, $func) {
+        $arrayMax = max($positions);
+        $leastFuel = PHP_INT_MAX;
+        $foundPos = 0;
+        for ($i = 0; $i < $arrayMax; $i++) {
+            $tempPositions = $positions;
+            array_walk($tempPositions, $func, $i);
+            $sum = array_sum($tempPositions);
+            if ($sum < $leastFuel) {
+                $leastFuel = $sum;
+                $foundPos = $i;
+            }
+        }
+
+        return $leastFuel;
     }
 
     public function sanitizeInput($input) {
