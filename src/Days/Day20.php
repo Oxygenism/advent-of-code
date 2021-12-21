ea9c7c40f293d083194ee3c3b24d5729ffcb7657
oxygenism <oxygenism@horchner.dev>
2021-12-21 05:55:16 +0100

    day20

--- src/Days/Day20.php
+++ src/Days/Day20.php
@@ -11,11 +11,11 @@ class Day20
     public $width;
     public const DIRECTIONS = [
         "upLeft" => [-1, -1],
-        "upRight" => [-1, 1],
         "up" => [-1, 0],
+        "upRight" => [-1, 1],
         "left" => [0, -1],
-        "right" => [0, 1],
         "self" => [0,0],
+        "right" => [0, 1],
         "downLeft" => [1, -1],
         "down" => [1, 0],
         "downRight" => [1, 1]
@@ -28,7 +28,7 @@ class Day20
 
     public function runA()
     {
-        return $this->run('day20_test.txt');
+        return $this->run('day20.txt');
     }
 
     public function runB()
@@ -38,43 +38,61 @@ class Day20
 
     public function run($file, $state = false) {
         $handle = $this->dataService->read($file);
-        $enhancementString = trim($handle->current());
+        $enhancementString = str_replace(['.', '#'], [0, 1], trim($handle->current()));
 
         $inputImage = "";
         $handle->next();
         $handle->next();
-        $this->width = strlen($handle->current());
-        $this->length = $handle->count();
+        $this->width = strlen(trim($handle->current()));
+        $this->length = $handle->count() - 2; //-2 for the first 2 lines in the input
 
         while ($handle->valid()) {
             $inputImage .= str_replace(['.', '#'], [0, 1],trim($handle->current()));
             $handle->next();
         }
 
-        $result = $this->getEnhancePos($inputImage, 0, 0);
+        for ($i = 0; $i < 50; $i++) {
+            $newInputImage = "";
+            for ($x = -1; $x < $this->length + 1; $x++) {
+                for ($y = -1; $y < $this->width + 1; $y++) {
+                    $pos = $this->getEnhancePos($inputImage, $x, $y);
+                    $newInputImage .= $enhancementString[$pos];
+                }
+            }
+            $inputImage = $newInputImage;
+            $this->width += 2;
+            $this->length += 2;
+        }
+//        $this->stringPrint($inputImage, $this->length, $this->width);
+        return array_sum(str_split($inputImage));
+    }
 
-        return "Only a bad programmer.";
+    public function stringPrint($string, $length, $width) {
+        for ($i = 0; $i < $length; $i++) {
+            for ($j = 0; $j < $width; $j++) {
+                echo $string[$this->findPos($i, $j)];
+            }
+            echo PHP_EOL;
+        }
     }
 
     public function getEnhancePos($image, $x, $y) {
         $binaryOutput = "";
         foreach (self::DIRECTIONS as $direction){
-            $directionX = $x - $direction[0];
-            $directionY = $y - $direction[1];
+            $directionX = $x + $direction[0];
+            $directionY = $y + $direction[1];
             $result = $this->findPos($directionX, $directionY);
-            if($result === false) {
-                $binaryOutput .= 0;
-            }
+            $binaryOutput .= $image[$result];
         }
 
         return bindec($binaryOutput);
     }
 
     public function findPos($x, $y) {
-        if ($x < 0 || $y < 0 || $x > $this->width || $x > $this->length) {
-            return false;
+        if ($x < 0 || $y < 0 || $y > $this->width - 1 || $x > $this->length - 1) {
+            return 0;
         }
 
-        return ($x * $this->width) + $y;
+        return $x * $this->width + $y;
     }
 }
